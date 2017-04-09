35c698c58c741e2b3755be738a9f91fb1fb44e8a
Dejan Acimovic <dejanacim@gmail.com>
2017-04-09 11:53:09 +0000

    Fix invalid login flash messages and add tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      flash[:danger] = 'Invalid e-mail/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid e-mail/password combination'
       render 'new'
     end
   end
